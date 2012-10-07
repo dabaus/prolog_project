2295d6f927b92cb68bf37c343e7cb55296da1d39
jakob <djgreiz@gmail.com>
2012-10-07 16:32:34 +0200

    Added def to map between regular implication in the prolog database and DCGs.

--- Jakob/misc/parsetree.pl
+++ Jakob/misc/parsetree.pl
@@ -5,7 +5,17 @@ verb_phrase(vp(V,NP)) --> transitive_verb(V), noun_phrase(NP).
 verb_phrase(vp(V)) --> intransitive_verb(V).
 determiner(every) --> [every].
 determiner(a) --> [a].
-noun(man) --> [man].
-noun(woman) --> [woman].
+noun(X) --> [X], {noun(X)}.
 proper_name(john) --> [john].
 transitive_verb(loves) --> [loves].
+
+noun(man).
+noun(men).
+noun(woman).
+noun(women).
+pluralOf(man, men).
+pluralOf(woman, women).
+isCountable(man).
+isCountable(women).
+
+main(Out) :- phrase(sentence(s(_,vp(loves,np(every,woman)))), Out).
