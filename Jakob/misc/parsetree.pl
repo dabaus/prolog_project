65aabd5b876b39cc1fb61e4c4f2ce7b73a3ed6db
jakob <djgreiz@gmail.com>
2012-10-07 20:59:07 +0200

    Find works now.

--- Jakob/misc/parsetree.pl
+++ Jakob/misc/parsetree.pl
@@ -1,14 +1,14 @@
 :- include('atomize.pl').
 
 statement((s, [NP,VP])) --> noun_phrase(NP), verb_phrase(VP).
-noun_phrase((np, (det, [D]), (n, [N]))) --> determiner(D), noun(N).
-noun_phrase((np, [(p,[P]),(n, [N])])) --> particle(P), noun(N).
-noun_phrase((np, [(n, [N])])) --> proper_name(N).
-noun_phrase((np, [(det, [D]), (ad, [AD]), (n, [N])])) --> determiner(D), adjective(AD), noun(N).
+noun_phrase((np, [(det, D), (n, N)])) --> determiner(D), noun(N).
+noun_phrase((np, [(p, P),(n, N)])) --> particle(P), noun(N).
+noun_phrase((np, [(n, N)])) --> proper_name(N).
+noun_phrase((np, [(det, D), (ad, AD), (n, N)])) --> determiner(D), adjective(AD), noun(N).
 %noun_phrase(np(PN)) --> pronoun(PN).
-verb_phrase((vp, [(v, [V], [NP])])) --> transitive_verb(V), noun_phrase(NP).
-verb_phrase((vp, [(v, [V]), (ad, [AD])])) --> transitive_verb(V), adjective(AD).
-verb_phrase((vp, [V])) --> intransitive_verb(V).
+verb_phrase((vp, [(v, V),(np, NP)])) --> transitive_verb(V), noun_phrase(NP).
+verb_phrase((vp, [(v, V), (ad, AD)])) --> transitive_verb(V), adjective(AD).
+verb_phrase((vp, V)) --> intransitive_verb(V).
 noun(X) --> [X], {noun(X)}.  
 determiner(X) --> [X], {determiner(X)}.
 proper_name(X) --> [X], {name(X)}. 
@@ -52,11 +52,11 @@ findElement(El, Tree, Out) :-
 findEl(El, [(El, L)|_], Out) :-
         Out = L.
 
+%Depth first branch
+findEl(El, [(_, L)|T], Out) :-
+        findEl(El, L, Out);
+        findEl(El, T, Out).
+
 findEl(_, [], _) :-
         fail.
-
-%Depth first recursion
-findEl(El, [(_, L)|Rest], Out) :-
-        findEl(El, L, Out),
-        findEl(El, Rest, Out).
         
