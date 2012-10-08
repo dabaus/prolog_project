cc820b8e23db387d1d238ced1c7499811eab0730
jakob <djgreiz@gmail.com>
2012-10-08 17:05:06 +0200

    the cat is red works.

--- Jakob/misc/parsetree.pl
+++ Jakob/misc/parsetree.pl
@@ -1,12 +1,16 @@
 :- include('atomize.pl').
 
+query((q,[IP, VP])) --> interogative_phrase(IP), verb_phrase(VP).
+interogative_phrase((ip, [(i, I)])) --> interogative(I).
+interogative(I) --> [I], {interogative(I)}.
+
 statement((s, [NP,VP])) --> noun_phrase(NP), verb_phrase(VP).
 noun_phrase((np, [(det, D), (n, N)])) --> determiner(D), noun(N).
 noun_phrase((np, [(p, P),(n, N)])) --> particle(P), noun(N).
 noun_phrase((np, [(n, N)])) --> proper_name(N).
 noun_phrase((np, [(det, D), (ad, AD), (n, N)])) --> determiner(D), adjective(AD), noun(N).
 %noun_phrase(np(PN)) --> pronoun(PN).
-verb_phrase((vp, [(v, V),(np, NP)])) --> transitive_verb(V), noun_phrase(NP).
+verb_phrase((vp, [(v, V), NP])) --> transitive_verb(V), noun_phrase(NP).
 verb_phrase((vp, [(v, V), (ad, AD)])) --> transitive_verb(V), adjective(AD).
 verb_phrase((vp, V)) --> intransitive_verb(V).
 noun(X) --> [X], {noun(X)}.  
@@ -36,30 +40,50 @@ transVerb(is).
 intransVerb(run).
 adjective(red).
 adjective(big).
+interogative(what).
+
+
 %main(Out) :- phrase(sentence(s(_,vp(loves,np(every,woman)))), Out).
 
+readStatement :-
+      write('Statement:'),
+      read_line(X),
+      atomize(X, Atoms),
+      phrase(statement(Stat), Atoms),
+      findElement(ad, Stat, Adjective),
+      findElement(v, Stat, Verb),
+      findElement(n, Stat, Noun),
+      findElement(p, Stat, Particle), 
+      assert(fact(Particle, Noun, Verb, Adjective)),
+      format('~w ~w ~w ~w ~n', [Particle, Noun, Verb, Adjective]).
+
+readQuery :-
+      write('Query:'),
+      read_line(X),
+      atomize(X, Atoms),
+      phrase(query(Query), Atoms),
+      findElement(n, Query, Noun),
+      findElement(p, Query, Particle),
+      findElement(v, Query, Verb),
+      fact(Particle, Noun, Verb, Adjective),
+      format('~w ~w ~w ~w ~n', [Particle, Noun, Verb, Adjective]).
+
 main :-
-        write('Statement:'),
-        read_line(X),
-        atomize(X, Atoms),
-        phrase(statement(Out), Atoms),
-        findElement(ad, Out, Adjective),
-        findElement(n, Out, Noun),
-        assert(fact(Noun, Adjective)),
-        write(Out).
- 
+       readStatement,
+       readQuery.
+        
 findElement(El, Tree, Out) :-
-        findEl(El, [Tree], Out).
+       findEl(El, [Tree], Out).
         
 %Element found
 findEl(El, [(El, L)|_], Out) :-
-        Out = L.
+       Out = L, !.
 
 %Depth first branch
 findEl(El, [(_, L)|T], Out) :-
-        findEl(El, L, Out);
-        findEl(El, T, Out).
+       findEl(El, L, Out);
+       findEl(El, T, Out).
 
 findEl(_, [], _) :-
-        fail.
+       fail.
         
