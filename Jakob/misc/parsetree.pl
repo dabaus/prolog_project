6761ac65266f21a4a51afb3a34f4a7e9f788aef1
jakob <djgreiz@gmail.com>
2012-10-09 19:41:59 +0200

    The stuff

--- Jakob/misc/parsetree.pl
+++ Jakob/misc/parsetree.pl
@@ -14,10 +14,13 @@ interogative((ig, I)) --> [I], {interogative(I)}.
 
 statement((s, [NP,VP])) --> noun_phrase(NP), verb_phrase(VP).
 noun_phrase((np, [D, N])) --> determiner(D), noun(N).
+noun_phrase((np, [N])) --> noun(N).
 noun_phrase((np, [D, AD, N])) --> determiner(D), adjective(AD), noun(N).
 noun_phrase(np(PN)) --> pronoun(PN).
+verb_phrase((vp, [V, Q, N])) --> transitive_verb(V), quantity(Q), noun(N).
 verb_phrase((vp, [V, NP])) --> transitive_verb(V), noun_phrase(NP).
 verb_phrase((vp, [V, AD])) --> transitive_verb(V), adjective(AD).
+
 verb_phrase((vp, V)) --> intransitive_verb(V).
 noun((n, X)) --> [X], {noun(X)}.  
 determiner((d, X)) --> [X], {determiner(X)}.
@@ -25,10 +28,14 @@ transitive_verb((v, X)) --> [X], {transVerb(X)}.
 intransitive_verb((v, X)) --> [X], {intransVerb(X)}.
 adjective((ad, X)) --> [X], {adjective(X)}.
 pronoun((p, X)) --> [X], {pronoun(X)}.
+quantity((q, X)) --> [X], {quantity(X)}.
 
 %main(Out) :- phrase(sentence(s(_,vp(loves,np(every,woman)))), Out).
 
 interogative(what).
+
+quantity(X) :-
+        number(X).
 
 transVerb(Conj) :-
         getVerb(Inf, Conj),
@@ -54,20 +61,65 @@ readQuery :-
 respond(Query) :-
         Query = (q, _),
         findElement(n, Query, Noun),
-        findElement(d, Query, Determiner),
         findElement(v, Query, Verb),
-        fact(Determiner, Noun, Verb, Adjective),
-        format('~w ~w ~w ~w ~n', [Determiner, Noun, Verb, Adjective]).
-
+        retrieve(Noun, Verb).
+             
 respond(Stat) :-
         Stat = (s, _),
         findElement(ad, Stat, Adjective),
         findElement(v, Stat, Verb),
         findElement(n, Stat, Noun),
-        findElement(d, Stat, Determiner),
-        assert(fact(Determiner, Noun, Verb, Adjective)),
-        format('~w ~w ~w ~w ~n', [Determiner, Noun, Verb, Adjective]).
+        assign_adj(Noun, Verb, Adjective).
 
+respond(Stat) ;-
+        Stat = (s, _),
+        findElement(np, Stat, VerbPhrase),
+        findElement(np, Stat, NounPhrase),
+        findElement(n, NounPhrase, Noun),
+        findElement(vp, Stat, VerbPhrase),
+        findElement(v, VerbPhrase, Verb),
+        findElement(q, VerbPhrase, Quantity),
+        findElement(n, VerbPhrase, TargetNoun),
+        assign_quant(Noun, Verb, Quantity, TargetNoun).
+        
+respond(Stat) :-
+        Stat = (s, _),
+        findElement(vp, Stat, VerbPhrase),
+        findElement(v, VerbPhrase, Verb),
+        findElement(n, Stat, Noun),
+        findElement(n, VerbPhrase, TargetNoun),
+        assign_noun(Noun, Verb, TargetNoun).
+        
+retrieve(Noun, Verb) :-
+        getVerb(be, Verb),
+        make(Noun, Adjective),
+        format("~w ~w ~w ~n", [Noun, Verb, Adjective]).
+retrieve(Noun, Verb) :-
+        getVerb(have, Verb),
+        give(Noun, Quantity, Adjective),
+        format("~w ~w ~w ~w ~n", [Noun, Verb, Quantity, Adjective]).
+retrieve(Noun, Verb) :-
+        getVerb(have, Verb),
+        give(Noun, Adjective),
+        format("~w ~w ~w ~n", [Noun, Verb, Adjective]).
+
+assign_adj(Noun, Verb, Adjective) :-
+        getVerb(be, Verb),
+        assert(make(Noun, Adjective)),
+        format("~w is ~w ~n", [Noun, Adjective]). 
+assign_noun(Noun, Verb, Target) :-
+        getVerb(be, Verb),
+        assert(child_of(Noun, Target)),
+        format("~w is a ~w ~n", [Noun, Target]).
+assign_noun(Noun, Verb, Target) :-
+        getVerb(have, Verb),
+        assert(give(Noun, 1, Target)),
+        format("~w has ~w ~n", [Noun, Target]). 
+assign_quant(Noun, Verb, Quantity, Target) :-
+        getVerb(have, Verb),
+        assert(give(Noun, Quantity, Target)),
+        format("~w has ~w ~w ~n", [Noun, Quantity, Target]).
+                        
 respond(Greet) :-
         Greet = (g, _),
         findElement(ij, Greet, Inter),
@@ -79,11 +131,14 @@ readSentence :-
         write('Input:'),
         read_line(X),
         atomize(X, Atoms),
-        phrase(sentence(Tree), Atoms),
+        phrase(sentence(Tree), Atoms),    
         respond(Tree).
 
 main :-
-       readSentence.
+       (readSentence,
+       main); 
+       (format('no clue man.~n', []),
+       main).
         
 findElement(El, Tree, Out) :-
        findEl(El, [Tree], Out).
